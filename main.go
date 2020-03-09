be9bca9eecccb690719a28631268652dfb53f6d9
Mike <michael.desson@gmail.com>
2020-03-09 13:59:38 -0400

    Created template

---
+++ main.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("vim-go")
+}
+
+var template = `
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Quiet Hacker News</title>
+</head>
+<body>
+	<h1>Quiet Hacker News</h1>
+<ol>
+	<li><a href="https://www.google.ca">Literally just google.com</a> <span class="source">google.ca</span></li>
+	<li><a href="https://www.yahoo.ca">Yahoo?</a> <span class="source">yahoo.ca</span></li>
+</body>
+</html>
+`
